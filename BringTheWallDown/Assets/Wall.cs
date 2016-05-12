e19044cdb1615621c1728badf288317104bf1a4e
Nantarat Channoi <taeza38007@hotmail.com>
2016-05-12 20:08:42 +0700

    Wall Fall and Destroyed

---
+++ BringTheWallDown/Assets/Wall.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wall : MonoBehaviour {
+
+	public FracturedObject fo;
+	public bool StaticOrNot;
+
+	void Start () {
+	
+	}
+	
+
+	void Update () {
+	
+		fo.StartStatic = StaticOrNot;
+
+	}
+}
---
+++ BringTheWallDown/Assets/WallFall.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallFall : MonoBehaviour 
+{
+	
+	private Vector3 Wallpos; 
+
+
+	void Start () 
+	{
+		
+	}
+	
+
+	void FixedUpdate ()
+	{
+
+		Vector3	Wallpos = transform.rotation.eulerAngles;
+
+		if (Wallpos.x == 90f) {
+			Destroy (this.gameObject);
+		}
+	}
+
+}
