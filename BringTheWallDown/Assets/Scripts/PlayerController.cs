0fe308debd3261a065567e7e0bb15c770db3a039
Nantarat Channoi <taeza38007@hotmail.com>
2016-05-12 19:22:15 +0700

    Basic Elements added
    Player Camera Wall

---
+++ BringTheWallDown/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed = 30f;
+	Rigidbody rb;
+
+	void Start () 
+	{
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+
+	void FixedUpdate(){
+
+
+		float moveLeft = Input.GetAxis ("Horizontal");
+		float moveUp = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3(moveLeft,0.0f,moveUp);
+
+		rb.AddForce (movement*speed);
+
+	}
+
+}
